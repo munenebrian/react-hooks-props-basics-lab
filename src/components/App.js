b5e01ee9f7b99564fd31ac23f5b9cddb64e3639b
munenebrian <njerubrianmunene@gmail.com>
2022-07-13 13:46:47 +0300

    new folder Links.js!

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
+//In the App component, pass down data from the user object as props to the 
+//Home component so that it renders correctly. 
+//There is some starter code for you in the Home component that should give you a 
+//clue as to what props this component takes in.
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +15,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
